2da7e0d2c8d91aee56c41faa7959dc707a1791cf
RYOTA2528 <1216022@gmail.com>
2021-07-25 08:31:40 +0000

    [Update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'top' =>'homes#top'
   root to: 'homes#top'
   get 'home/about'  =>'abouts#top'
-  
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+  end
+  resources :users, only: [:index, :show, :edit, :update]
 end
